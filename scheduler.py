cdb70e058c0b57f8f88d98d2058d471a9ea4b876
yotammarton <yonam92@gmail.com>
2022-12-04 20:58:50 +0200

    Add scheduler in order to make scheduled POST calls to the backend - Upload to S3

--- scheduler.py
+++ scheduler.py
@@ -6,6 +6,11 @@ TODO [YOTAM] this mechanism should be improved !!
 import time
 import json
 import requests
+import os
+
+from files import *
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 
 filepath = ''
 
@@ -16,6 +21,22 @@ with open(filepath + 'config.json', encoding="utf8") as f:
 f.close()
 POST_INFERENCE_INTERVAL_SECONDS = config['POST_INFERENCE_INTERVAL_SECONDS']
 BACKEND_URL = config['BACKEND_URL']
+OUTPUT_FOLDER = config['OUTPUT_FOLDER']
+
+with open(filepath + 'credentials.json') as f:
+    file = json.load(f)
+
+# Closing json file
+f.close()
+
+# Access key to AWS. Write here your aws credentials
+AWS_ACCESS_KEY_ID = file['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = file['AWS_SECRET_ACCESS_KEY']
+
+# S3 bucket name. Write here your s3 bucket name
+BUCKET = file['BUCKET']
+
+s3 = Files(s3_bucket=BUCKET, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
 
 def call_get_internally_upload_to_s3_and_make_post_request_to_backend():
@@ -33,5 +54,14 @@ while True:
     detection_results = requests.get(url='http://localhost:5000/detect_trucks')
     detection_results = json.loads(detection_results.text)
 
-    print(detection_results)
+    # Upload images to S3
+    absolute_paths_to_images = [
+        os.path.join(SCRIPT_DIR, OUTPUT_FOLDER, image_id + '.jpg')
+        for image_id in detection_results['image_id']
+    ]
+    print(absolute_paths_to_images)  # TODO DELETE
+    for path in absolute_paths_to_images:
+        s3.upload_to_s3_from_local(path)
+
+    # TODO Add the call to the backend and make sure it works smoothly
     # response = requests.post(BACKEND_URL, json=users_data)
